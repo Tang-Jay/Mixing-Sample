8e2d27b779db3464595ae593819aece12d9fe733
Tang-Jay <1243711126@qq.com>
2022-09-02 10:00:11 +0800

    2022-09-02

--- Figures/f.R
+++
@@ -1,4 +0,0 @@
-f <- function(x, mu = 0, sigma2 = 1){
-  fx = (2*pi*sigma2)^(-0.5)*exp(-(x-mu)^2/(2*sigma2))
-  return(fx)
-}
--- README.md
+++
@@ -1,7 +0,0 @@
-中文：
-
-这里是一些论文相关代码。
-
-English:
-
-There are some R codes related to the article.
